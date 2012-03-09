950b69fccfd9e2448cd613438a40fdf91d2d2282
Gabriel Ames-Bull <g.ames-bull@tangente.co>
2012-03-09 13:08:20 -0500

    Initial Commit

---
+++ jquery.center-align.js
@@ -0,0 +1,43 @@
+/**
+ * jQuery.centerAlign
+ * Dual licensed under MIT and GPL.
+ * Date: 03/09/2012
+ * @author Gabriel Bull
+ * @version 0.1
+ *
+ * https://github.com/gavroche/jquery-centeralign
+ */
+(function($) {
+    var methods = {
+        /* Init plugin */
+        init : function( options ) {
+            return this.each(function(){
+                var obj = this;
+ 
+                var settings = $.extend( {
+                    'horizontal': true,
+                    'vertical': true
+                }, options);
+                 
+                if (settings.vertical) {
+                    $(this).css('top', ((($(document.body).height()/2) - ($(this).height() / 2))+'px'));
+                }
+                if (settings.horizontal) {
+                    $(this).css('left', ((($(document.body).width()/2) - ($(this).width() / 2))+'px'));
+                }
+            });
+        }
+    };
+         
+    $.fn.centeralign = function( method ) {
+        // Method calling logic
+        if (methods[method]) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.uploader' );
+        }
+    };
+ 
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,12 @@
+# jQuery Center Align
+
+Align an element to the center of the screen.
+
+## Example Usage
+
+`$('#element').centeralign();`
+
+You can specify if whether you want the horizontal or the vertical alignment, like in this example:
+
+`$('#element').centeralign({'vertical': false});`
+`$('#element').centeralign({'horizontal': false});`
