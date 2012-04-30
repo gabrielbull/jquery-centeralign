e3ee5f867bdbfbebe4c1822d61d0625123f0f6b1
Gabriel Bull <gavroche.bull@gmail.com>
2012-04-30 18:28:59 +0000

    Now updates the object position to relative if it is not relative, fixed or absolute

--- jquery.centeralign.js
+++ jquery.centeralign.js
@@ -7,37 +7,42 @@
  *
  * https://github.com/gavroche/jquery-centeralign
  */
+
 (function($) {
-    var methods = {
-        /* Init plugin */
-        init : function( options ) {
-            return this.each(function(){
-                var obj = this;
- 
-                var settings = $.extend( {
-                    'horizontal': true,
-                    'vertical': true
-                }, options);
-                 
-                if (settings.vertical) {
-                    $(this).css('top', ((($(document.body).height()/2) - ($(this).height() / 2))+'px'));
-                }
-                if (settings.horizontal) {
-                    $(this).css('left', ((($(document.body).width()/2) - ($(this).width() / 2))+'px'));
-                }
-            });
-        }
-    };
-         
-    $.fn.centeralign = function( method ) {
-        // Method calling logic
-        if (methods[method]) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.uploader' );
-        }
-    };
- 
+   "use strict";
+   var methods = {
+		/* Init plugin */
+		init : function( options ) {
+			return this.each(function(){
+                var settings = $.extend({
+                    'horizontal': true, 
+                    'vertical': true
+				}, options);
+                
+				var position = $(this).css('position');
+                if (position!=='absolute' && position!=='relative' && position!=='fixed') {
+                    $(this).css('position', 'relative');
+                }
+                
+				if (settings.vertical) {
+					$(this).css('top', ((($(document.body).height()/2) - ($(this).height() / 2))+'px'));
+				}
+				if (settings.horizontal) {
+					$(this).css('left', ((($(document.body).width()/2) - ($(this).width() / 2))+'px'));
+				}
+			});
+		}
+	};
+
+	$.fn.centeralign = function( method ) {
+		// Method calling logic
+		if (methods[method]) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.uploader' );
+		}
+	};
+
 })(jQuery);
